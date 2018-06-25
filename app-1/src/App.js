3a347dd18b0ad2df258a34ef0383eacc68b84e8c
blermyjake <jeremy73120@gmail.com>
2018-06-25 16:17:49 -0500

    app-1

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,19 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
+
 class App extends Component {
+
+  constructor(){
+    super();
+
+    this.state = {
+      greeting: ""
+    }
+  }
+
+updateBasicText(val){
+  this.setState({greeting: val});
+}
+
+
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <input onChange={ (e) => this.updateBasicText( e.target.value )} type ="text"/>
+        <p>{ this.state.greeting } </p>
+
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        
+      
     );
   }
 }
