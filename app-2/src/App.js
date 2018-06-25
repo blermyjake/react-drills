f5028a462140cee666664991ce34c3cf2b201185
blermyjake <jeremy73120@gmail.com>
2018-06-25 17:34:42 -0500

    add-2

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -3,17 +3,56 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+constructor(){
+  super();
+
+this.state = {
+  list: [
+    'Sperghertti', 
+    'Ice Crame', 
+    'Sooshee', 
+    'Balog-knee', 
+    'Kimchi'
+  ]
+}
+}
+// copy of list in function
+// 
+
+
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+    // let showList = this.state.list.map(function(elem, index){
+    //   return (
+    //     <div key={index}>
+    //       <h2>{elem}</h2>
+    //     </div>
+    //   )
+    // })
+    
+    let showList = this.state.list.map((e, i) => {
+      return (
+        <div key={i}>
+          <h2>{e}</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      )
+    })
+    
+    
+      return (
+      
+      <div className="App">
+          { showList }
+        </div>
+        // <div>
+        //   <h2>{this.state.list[0]}</h2>
+        //   <h2>{this.state.list[1]}</h2>
+        //   <h2>{this.state.list[2]}</h2>
+        //   <h2>{this.state.list[3]}</h2>
+        //   <h2>{this.state.list[4]}</h2>
+        
+        // </div>
+      
     );
   }
 }
